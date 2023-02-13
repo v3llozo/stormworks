278a08421f19fd54ea693f6a36cff25aa988f7ef
Vinicius Vellozo <vinicius.vellozo@hotmail.com>
2023-02-13 08:51:51 -0300

    radar target position

--- radar.lua
+++ radar.lua
@@ -25,8 +25,16 @@ do
         simulator:setInputBool(1, screenConnection.isTouched)
         simulator:setInputNumber(2, screenConnection.touchX)
         simulator:setInputNumber(3, screenConnection.touchY)
-        simulator:setInputNumber(4, ticks / 10)
+        simulator:setInputNumber(4, ticks / 300)
 
+        simulator:setInputNumber(5, 150)
+        simulator:setInputNumber(6, 0.1215)
+        simulator:setInputNumber(7, -0.2)
+        simulator:setInputNumber(8, 0.5)
+        simulator:setInputNumber(9, 1400)
+        simulator:setInputNumber(10, 0.015)
+        simulator:setInputNumber(11, 0.8)
+        simulator:setInputNumber(12, 1)
         -- NEW! button/slider options from the UI
         simulator:setInputBool(1, simulator:getIsToggled(1)) -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
 
@@ -49,6 +57,9 @@ ticks = 0
 gB = input.getBool
 gN = input.getNumber
 maxTargets = 7
+-- 0.5km, 1km, 2km, 5km, 10km, 15km, 30km, 60km
+distanceDetector = { 0.002, 0.001, 0.0005, 0.0002, 0.0001, 0.000066, 0.0000333, 0.0000166 }
+selectedDistance = distanceDetector[7]
 function onTick()
     isTouched = gB(1)
     touchX = gN(2)
@@ -69,31 +80,50 @@ function onTick()
     end
 end
 
+detectedTargets = {}
 function onDraw()
     w = screen.getWidth()
     h = screen.getHeight()
-    r = (h / 2) * 0.9
+    r = (h / 2) * 0.95
     rX = r * math.sin(radar.angle * 0.0175) + h / 2
     rY = r * math.cos(radar.angle * 0.0175) + w / 2
 
+    for k, t in pairs(detectedTargets) do
+        if radar.angle < t.angle then
+            screen.setColor(255, 0, 0)
+            screen.drawRect(t.y, t.x, 1, 1)
+        else
+            table.remove(detectedTargets,k)
+        end
+    end
+
     screen.setColor(255, 255, 255)
-    screen.drawCircleF(w / 2, h / 2, 6)
-    screen.drawText(0, 0, string.format("%3.0f", r))
-    screen.drawText(0, 8, string.format("%3.2f", radar.angle))
-    screen.drawText(0, 16, string.format("%3.2f", rX))
-    screen.drawText(0, 24, string.format("%3.2f", rY))
+    screen.drawRectF(w / 2 - 1, h / 2 - 1, 3, 3)
+    screen.drawText(2, 0, string.format("%3.0f", r))
+    screen.drawText(2, 8, string.format("%3.2f", radar.angle))
+    screen.drawText(2, 16, string.format("%3.2f", rX))
+    screen.drawText(2, 24, string.format("%3.2f", rY))
+    screen.drawText(w - 38, 0, string.format("%.1f", (1 / selectedDistance) / 1000) .. "km")
+
 
     screen.setColor(40, 255, 40)
     screen.drawCircle(w / 2, h / 2, r)
     screen.drawLine(w / 2, h / 2, rY, rX)
+
+    targetAngle = radar.angle
     for i = 1, maxTargets, 1 do
         if radar.found[i] then
+            output.setBool(1,true)
             screen.setColor(255, 255, 255)
-            tX = (radar.target[i].distance * 0.00001666) * r * math.sin(radar.angle * 0.0175) + h / 2
-            tY = (radar.target[i].distance * 0.00001666) * r * math.cos(radar.angle * 0.0175) + w / 2
-            screen.drawText(0, 32 + i * 8, string.format("%1.2f", radar.target[i].azimuth))
+            tX = (radar.target[i].distance * selectedDistance) * r *
+                math.sin((targetAngle - radar.target[i].azimuth / 100) * 0.0175) + h / 2
+            tY = (radar.target[i].distance * selectedDistance) * r *
+                math.cos((targetAngle - radar.target[i].azimuth / 100) * 0.0175) + w / 2
+            screen.drawText(2, 32 + i * 8, string.format("%.0f", radar.target[i].distance))
+            screen.drawTextBox(w - 38, 32 + i * 8, 36, 8, string.format("%.5f", radar.target[i].azimuth), -1, 0)
             screen.setColor(255, 0, 0)
-            screen.drawRect(tY,tX, 3, 3)
+            screen.drawRect(tY, tX, 1, 1)
+            table.insert(detectedTargets, { y = tY, x = tX, angle = targetAngle })
         end
     end
 end

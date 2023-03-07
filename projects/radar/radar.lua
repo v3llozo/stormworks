0119634efd54ebee27f04f5a49e6fbbc8f68d6ca
Vinicius Vellozo <vinicius.vellozo@hotmail.com>
2023-03-06 21:11:03 -0300

    radar v1.0

--- projects/radar/radar.lua
+++ projects/radar/radar.lua
@@ -1,9 +1,12 @@
 --- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
 --- If you have any issues, please report them here: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension/issues - by Nameous Changey
---[====[ HOTKEYS ]====] -- Press F6 to simulate this file
+--[====[ HOTKEYS ]====]
+-- Press F6 to simulate this file
 -- Press F7 to build the project, copy the output from /_build/out/ into the game to use
 -- Remember to set your Author name etc. in the settings: CTRL+COMMA
---[====[ EDITABLE SIMULATOR CONFIG - *automatically removed from the F7 build output ]====] ---@section __LB_SIMULATOR_ONLY__
+--[====[ EDITABLE SIMULATOR CONFIG - *automatically removed from the F7 build output ]====]
+--
+--- @section __LB_SIMULATOR_ONLY__
 do
     ---@type Simulator -- Set properties and screen sizes here - will run once when the script is loaded
     simulator = simulator
@@ -20,7 +23,6 @@ do
         simulator:setInputNumber(2, screenConnection.touchX)
         simulator:setInputNumber(3, screenConnection.touchY)
         simulator:setInputNumber(4, ticks / 300)
-
         if (ticks / 300 % 1) then
             simulator:setInputNumber(5, 1000)
             simulator:setInputNumber(6, 0)
@@ -32,7 +34,7 @@ do
         simulator:setInputNumber(11, 0.8)
         simulator:setInputNumber(12, 1)
         -- NEW! button/slider options from the UI
-        simulator:setInputBool(1, simulator:getIsToggled(1)) -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
+        -- simulator:setInputBool(1, simulator:getIsClicked(1)) -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
     end
 end
 ---@endsection
@@ -45,7 +47,7 @@ ticks = 0
 maxTargets = 7
 defaultDistance = 3
 -- 0.5km, 1km, 2km, 5km, 10km, 15km, 30km, 60km
-distanceDetector = {0.002, 0.001, 0.0005, 0.0002, 0.0001, 0.0000666, 0.0000333, 0.0000166}
+distanceDetector = { 0.002, 0.001, 0.0005, 0.0002, 0.0001, 0.0000666, 0.0000333, 0.0000166 }
 selectedDistance = distanceDetector[defaultDistance]
 function getRaiousPosition(r, angle, h, w)
     return {
@@ -53,6 +55,7 @@ function getRaiousPosition(r, angle, h, w)
         y = r * math.cos(angle * 0.0175) + w / 2
     }
 end
+
 function onTick()
     output.setBool(1, false)
     isTouched = input.getBool(1)
@@ -81,20 +84,13 @@ function onTick()
 end
 
 detectedTargets = {}
-function getRadiusPosition(rad, angle, h, w)
-    return {
-        x = rad * math.sin(angle * 0.0175) + h / 2,
-        y = rad * math.cos(angle * 0.0175) + w / 2
-    }
-end
 function onDraw()
     w = screen.getWidth()
     h = screen.getHeight()
     screen.setColor(5, 4, 8)
     screen.drawRectF(0, 0, w, h)
-    r = w
-    rX = r * math.sin(radar.angle * 0.0175) + h / 2
-    rY = r * math.cos(radar.angle * 0.0175) + w / 2
+    r = h
+    rX, rY = getRaiousPosition(r, radar.angle, h, w)
 
     if isTouched then
         if defaultDistance >= 7 then
@@ -117,11 +113,6 @@ function onDraw()
     for i = 1, 10, 1 do
         screen.drawCircle(w / 2, h / 2, r / 10 * i)
     end
-    for i = 0, 7, 1 do
-        screen.drawLine(w / 2, h / 2, r * math.cos((i * 45) * 0.0175) + w / 2, r * math.sin((i * 45) * 0.0175) + h / 2)
-    end
-
-    -- radar
     screen.setColor(40, 200, 40, 80)
     r1 = getRaiousPosition(r, radar.angle - 20, h, w)
     r2 = getRaiousPosition(r, radar.angle, h, w)
@@ -132,9 +123,9 @@ function onDraw()
         if radar.found[i] then
             screen.setColor(255, 255, 255)
             tX = (radar.target[i].distance * selectedDistance) * r *
-                     math.sin((targetAngle - radar.target[i].azimuth / 100) * 0.0175) + h / 2
+                math.sin((targetAngle - radar.target[i].azimuth / 100) * 0.0175) + h / 2
             tY = (radar.target[i].distance * selectedDistance) * r *
-                     math.cos((targetAngle - radar.target[i].azimuth / 100) * 0.0175) + w / 2
+                math.cos((targetAngle - radar.target[i].azimuth / 100) * 0.0175) + w / 2
             screen.setColor(255, 10, 20)
             screen.drawRect(tY, tX, 1, 1)
             table.insert(detectedTargets, {
@@ -145,16 +136,15 @@ function onDraw()
             })
         end
     end
-
     for k, t in pairs(detectedTargets) do
         screen.setColor(220, 10, 20)
         screen.drawRect(t.y, t.x, 1, 1)
     end
-
     screen.setColor(255, 255, 255, 255)
     screen.drawRectF(w / 2 - 1, h / 2 - 1, 3, 3)
     screen.setColor(3, 3, 5)
-    screen.drawRectF(0, h - 8, 21, 8)
+    screen.drawRectF(0, h - 9, 23, 9)
     screen.setColor(255, 255, 255, 255)
-    screen.drawText(1, h - 6, string.format("%1.1f", 1 / distanceDetector[defaultDistance] / 1000))
+    screen.drawRect(0, h - 9, 22, 8)
+    screen.drawTextBox(1, h - 8, 22, 8, string.format("%1.1f", 1 / distanceDetector[defaultDistance] / 1000), 0, 0)
 end

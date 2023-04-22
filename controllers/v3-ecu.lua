0f09364198c468b02aba983cc59e1b7eaa13180c
Vinicius Vellozo <26423669+v3llozo@users.noreply.github.com>
2023-04-22 15:55:19 -0300

    feature: start/stop ignition

--- controllers/v3-ecu.lua
+++ controllers/v3-ecu.lua
@@ -7,6 +7,7 @@ function clamp(n, min, max)
 	end
 	return n
 end
+
 --@endsection
 
 --@section pid
@@ -30,10 +31,8 @@ function pid(p, i, d)
 		end
 	}
 end
---@endsection
 
-function onDraw()
-end
+--@endsection
 
 rpsIdle = pid(.4, .0005, .07)
 rpsLimit = pid(.8, .0001, 0)
@@ -41,6 +40,7 @@ tickCounter = 0
 
 x = 0.0001
 y = 0.0001
+startStopToggle = false
 
 function onTick()
 	if tickCounter > 60000 then tickCounter = 0 end
@@ -57,33 +57,42 @@ function onTick()
 	gear = input.getNumber(10)
 	startStop = input.getBool(1)
 	onOff = input.getBool(2)
+	overheat = input.getBool(3)
 
-	if gear == 0 then throttle = clamp(throttle, 0, 0.25) end
+	if startStop == true then startStopToggle = not startStopToggle end
 
-	pidmin = clamp(rpsIdle:run(rps, minRps) * -1, 0, 1)
-	pidmax = clamp(rpsLimit:run(rps, maxRps) * -1, throttle * -0.85, 0)
-	throttle = throttle + pidmax
-	if rps > 1 then
-		if rps < minRps then throttle = throttle + pidmin end
+	if onOff == false or (onOff == true and startStopToggle == false) then
+		throttle = 0
+		airIntake = 0
+		fuelIntake = 0
+	else
+		if gear == 0 then throttle = clamp(throttle, 0, 0.25) end
+
+		pidmin = clamp(rpsIdle:run(rps, minRps) * -1, 0, 1)
+		pidmax = clamp(rpsLimit:run(rps, maxRps) * -1, throttle * -0.8, 0)
+		throttle = throttle + pidmax
+		if rps > 1 then
+			if rps < minRps then throttle = throttle + pidmin end
+		end
+
+		if air == 0 then air = 0.0001 end
+		if fuel == 0 then fuel = 0.0001 end
+		if stoichometric == 0 then stoichometric = 0.5 end
+		if sensibility == 0 then sensibility = 0.02 end
+
+		x = ((14 - (stoichometric * 2)) * (1 - temp)) + ((15 - (stoichometric * 5)) * temp)
+		y = clamp(y + ((1 - (x / ((air * 1000) / (fuel * 1000)))) * sensibility), -1, 1)
+		airIntake = throttle * clamp(1 - y, 0.1, 1)
+		fuelIntake = throttle * clamp(1 + y, 0.1, 1)
 	end
-
-	if air == 0 then air = 0.0001 end
-	if fuel == 0 then fuel = 0.0001 end
-	if stoichometric == 0 then stoichometric = 0.5 end
-	if sensibility == 0 then sensibility = 0.02 end
-
-	x = ((14 - (stoichometric * 2)) * (1 - temp)) + ((15 - (stoichometric * 5)) * temp)
-	y = clamp(y + ((1 - (x / ((air * 1000) / (fuel * 1000)))) * sensibility), -1, 1)
-	airIntake = throttle * clamp(1 - y, 0.1, 1)
-	fuelIntake = throttle * clamp(1 + y, 0.1, 1)
 
 	output.setNumber(1, airIntake)
 	output.setNumber(2, fuelIntake)
 	output.setNumber(3, pidmin)
 	output.setNumber(4, pidmax)
-	output.setNumber(5, temp)
+	output.setNumber(5, temp * 100)
 	output.setNumber(6, rps)
 
-	output.setBool(1, startStop)
+	output.setBool(1, startStopToggle)
 	output.setBool(2, onOff)
 end
